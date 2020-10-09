f83e95b701d855524474462c2a80e6b2824d723a
Sram-Tools <github@xeth.de>
2020-10-09 17:23:34 +0200

    set project url spelling

--- Models/Enums/Unknown/Unknown15_IvoryTowerFlags_Offset20.cs
+++ Models/Enums/Unknown/Unknown15_IvoryTowerFlags_Offset20.cs
@@ -12,6 +12,6 @@ namespace SramFormat.SoE.Models.Enums
 		Level3_House3_Floor1_RearEntry_LeftChest__AnnihilationAmulet = 0b0000_0100, // bit 3  (U12C flag 0000-b1)
 		Level3_House3_Floor1_RearEntry_CenterChest__100GM = 0b0000_1000, // bit 4
 
-		//Level3_House3_Floor1_RearEntry_RightChest__3xAcorns = 0b0000_1000, // bit 4 or 5? // Needs to be proofed again
+		//Level3_House3_Floor1_RearEntry_RightChest__3xAcorns = 0b0000_1000, // bit 4 or 5? // Needs to be proved again
 	}
 }
